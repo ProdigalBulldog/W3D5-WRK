5dc458c07371fd7d08316bf8b1714465256ba670
ProdigalBulldog <michael.schwartz@aya.yale.edu>
2017-11-24 15:33:07 -0500

    Complete my_attr_accessor warmup

--- lib/00_attr_accessor_object.rb
+++ lib/00_attr_accessor_object.rb
@@ -1,5 +1,20 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+    names.each do |insta_var_sym|
+      getter_sym = ("@" + insta_var_sym.to_s).to_sym
+      setter_sym = (insta_var_sym.to_s + "=").to_sym
+      
+      define_method(insta_var_sym) { instance_variable_get(getter_sym) }
+      define_method(setter_sym) { |val| instance_variable_set(getter_sym, val) }
+      
+    end
   end
+  
+  # def self.my_attr_accessor(*names)
+  #   names.each do |inst_var_name|
+  #     define_method(inst_var_name) { instance_variable_get("@#{inst_var_name}") }
+  #     define_method("#{inst_var_name}=") { |val| instance_variable_set("@#{inst_var_name}", val) }
+  #   end
+  # end
+  
 end
